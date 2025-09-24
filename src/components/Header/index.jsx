118f5af60473ec10c709e6ac5e9e7f983bb3ff3a
Rafael <rafael.aguiar91@gmail.com>
2025-09-24 14:32:46 -0300

    feat/header: construção da parte visual do header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,33 @@
 import './styles.scss';
+import Brazil from '../../assets/brazil.svg';
+import USA from '../../assets/usa.svg'
 
 function Header() {
 
+  let language = "pt";
+
   return (
     <div className="container-header">
-        <h1>Header</h1>
+      <h1>Rafael Aguiar</h1>
+
+      <div className='header-right'>
+        <ul>
+          <li>
+            <a href='#about-section'>Sobre</a>
+          </li>
+          <li>
+            <a href='#experience-section'>Experiências</a>
+          </li>
+          <li>
+            <a href='#projects-section'>Projetos</a>
+          </li>
+        </ul>
+        <div className='languages'>
+          <img src={Brazil} alt='portuguese' className={language == "pt" ? "active" : ""} />
+          <img src={USA} alt='english' className={language === "en" ? "active" : ""}
+          />
+        </div>
+      </div>
     </div>
   )
 }
