c6cd6e864e48ab1d4b9068db1f393316ddf478ce
Rafael <rafael.aguiar91@gmail.com>
2025-09-25 16:26:55 -0300

    feat/about: estrutura inicial do componente about

---
+++ src/components/About/index.jsx
@@ -0,0 +1,12 @@
+import './styles.scss';
+
+function Header() {
+  return (
+    <div className="container-about">
+      <h1>SOBRE!</h1>
+
+    </div>
+  )
+}
+
+export default Header
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
 import Header from "../../components/Header"
+import About from "../../components/About";
 
 function Home() {
   return (
     <>
       <Header />
+      <About />
     </>
   )
 }
