f37995a27daa83262215fa50216ae62ece7ae862
Rafael <rafael.aguiar91@gmail.com>
2025-10-10 17:36:49 -0300

    style/about: ajuste de texto

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -16,7 +16,7 @@ function Header() {
         <div className='content-text'>
           <h1>{language == "pt" ? "Olá, sou o Rafael" : "Hello, I'm Rafael"}👋</h1>
           <p>{ language == "pt" ? 
-          "Desenvolvedor desde 2021, focado em criar soluções que proporcionem a melhor experiência para o usuário. Tenho facilidade em me adaptar a novas tecnologias e de transformar desafios em oportunidades de aprendizado, sempre aprendendo e evoluindo para resolver problemas que tenham impactos positivos para as pessoas." :
+          "Desenvolvedor desde 2021, focado em criar soluções que proporcionem a melhor experiência para o usuário. Tenho facilidade em me adaptar a novas tecnologias e transformo desafios em oportunidades de aprendizado, sempre aprendendo e evoluindo para resolver problemas que tenham impactos positivos para as pessoas." :
           "Developer since 2021, focused on creating solutions that provide the best user experience. I adapt easily to new technologies and enjoy turning challenges into learning opportunities, constantly evolving to solve problems that have a positive impact on people." }
           </p>
         </div>
